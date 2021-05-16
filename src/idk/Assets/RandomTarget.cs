370e198b72f170a5a8bb6c2d0662e6bc23aa6624
Cr4zi <61344888+Cr4zi@users.noreply.github.com>
2021-05-16 17:56:54 +0300

    Creating script that randomly generating the target location

--- src/idk/Assets/RandomTarget.cs
+++ src/idk/Assets/RandomTarget.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class RandomTarget : MonoBehaviour
 {
+    
+    public int target_amount = 3;
+    public Transform target;
+
+    private int x, y;
+
+
+    void SpawnRandom()
+    {
+        x = Random.Range(-1, 3);
+        y = Random.Range(1, 3);
+        //randomInt = Random.Range(0, spawnees.Length);
+        Instantiate(target, new Vector3(x, y, 2), target.rotation);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 3; i++)
+        {
+            SpawnRandom();
+        }
     }
 
     // Update is called once per frame
